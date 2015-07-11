9e17c6a67f34a6d012dc266f0ea95e0ade0e3028
Claudio Sanches <contato@claudiosmweb.com>
2015-07-11 16:18:04 -0300

    Updated the setup.py

---
+++ woocommerce-api/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+""" Python wrapper for WooCommerce API """
+
+__title__ = 'woocommerce-api'
+__version__ = '1.0.0'
+__author__ = 'Claudio Sanches @ WooThemes'
+__license__ = 'MIT'
+
+def hello():
+    """ Just a test """
+    return "test"
