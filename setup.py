99d0c8afa5a07de9abe2c0b5a205003436994755
Stefan Hospes <shospes@gmail.com>
2016-11-08 12:20:36 +0100

    Add Python 3.5 support

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ if not VERSION:
 README = open(os.path.join(os.path.dirname(__file__), "README.rst")).read()
 
 # allow setup.py to be run from any path
-os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+#os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name="WooCommerce",
@@ -35,7 +35,7 @@ setup(
     include_package_data=True,
     platforms=['any'],
     install_requires=[
-        "requests",
+        "aiohttp",
         "ordereddict"
     ],
     classifiers=(
